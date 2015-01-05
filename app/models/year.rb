0783f674520bb505a2ea33e587336919a0957128
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-01-05 05:06:24 +0000

    added issues to years with joins

--- app/models/year.rb
+++ app/models/year.rb
@@ -14,4 +14,8 @@ class Year < ActiveRecord::Base
 
   # Validations
   validates :year, presence: true, uniqueness: true
+  
+  def articles
+    Article.joins("INNER JOIN issues ON articles.issue_id=issues.id").joins("INNER JOIN years ON issues.year_id=years.id").where("year_id = ?", id)
+  end
 end
