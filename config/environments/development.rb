0f84ac51222d1a3dbe1a651474a841f6f49d3004
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-10-28 09:05:08 -0400

    bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.after_initialize do
+    #Enable bullet in your application
+    Bullet.enable       = true
+    Bullet.console      = true
+    Bullet.rails_logger = true
+  end
 end
