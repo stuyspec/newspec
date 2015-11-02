27ca1551031aa411231b9636d62237c783ec79a8
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-11-02 15:23:08 -0500

    Author.create_author with unique slug

--- app/models/author.rb
+++ app/models/author.rb
@@ -9,10 +9,20 @@ class Author < ActiveRecord::Base
     def is_unique_slug(slug)
       not Author.exists?(slug: slug)
     end
+
+    def create_author(first, last)
+      create!(first: first, last: last) do |author|
+        author.slug = UniqueAuthorSlug.call(author)
+      end
+    end
   end
 
   def name
     "#{first} #{last}"
+  end
+
+  def to_param
+    slug
   end
 
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
 
   describe ".is_unique_slug" do
-
     it "is true when slug is untaken" do
       create(:author, first: "Jake", last: "Waksbaum", slug: "AHHAHAHA")
       expect(Author.is_unique_slug("jake-waksbaum")).to be true
@@ -13,7 +12,18 @@ RSpec.describe Author, type: :model do
       create(:author, first: "foo", last: "blah", slug: "jake-waksbaum")
       expect(Author.is_unique_slug("jake-waksbaum")).to be false
     end
+  end
 
+  describe ".create_author" do
+    it "creates an author when slug is untaken" do
+      Author.create_author("Jake", "Waksbaum")
+    end
+
+    it "creates an author when the slug is taken" do
+      2.times do
+        Author.create_author("Jake", "Waksbaum")
+      end
+    end
   end
 
 end
