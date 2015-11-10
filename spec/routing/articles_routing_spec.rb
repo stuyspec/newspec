f18593f8c2caa28e239f63d876216f4bd9861f77
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-11-10 18:00:19 -0500

    issue_num instead of issue in routes

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -1,19 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :routing do
-  describe "routing" do
-
-    it "routes to #by_year" do
-      expect(:get => "/2016").to route_to("articles#by_year", :year => "2016")
+  describe 'routing' do
+    it 'routes to #by_year' do
+      expect(get: '/2016').to route_to('articles#by_year', year: '2016')
     end
 
-    it "routes to #by_issue" do
-      expect(:get => "/2016/1").to route_to("articles#by_issue", :year => "2016", :issue => "1")
+    it 'routes to #by_issue' do
+      expect(get: '/2016/1').to route_to('articles#by_issue',
+                                         year: '2016',
+                                         issue_num: '1')
     end
 
-    it "routes to #by_slug" do
-      expect(:get => "/2016/1/my-cool-article").to route_to("articles#by_slug", :year => "2016", :issue => "1", :slug => "my-cool-article")
+    it 'routes to #by_slug' do
+      expect(get: '/2016/1/cool-article').to route_to('articles#by_slug',
+                                                      year: '2016',
+                                                      issue_num: '1',
+                                                      slug: 'cool-article')
     end
-
   end
 end
